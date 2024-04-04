c9a3598813b5ac27ed64407222102cfb1f7cb245
Diminas <pilluping@gmail.com>
2024-04-04 15:52:02 +0300

    +pgsfile

--- PythonPostgreServer/main.py
+++ PythonPostgreServer/main.py
@@ -1,4 +1,19 @@
-import flask
+from flask import Flask, wrappers, request, jsonify
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
+
+@app.route("/person", methods=['POST'])
+def get_person() -> tuple[wrappers.Response, int]:
+    # Получаем данные из тела запроса в формате JSON
+    request_data = request.get_json()
+
+    # Извлекаем значение титла из полученных данных
+    login: str = request_data.get('login')
+    password: str = request_data.get('password')
+    return jsonify({"ok": "ok"}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
