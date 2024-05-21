55359d77ebcc510ec660f4435a44351303ce3979
Diminas <pilluping@gmail.com>
2024-05-22 00:11:56 +0300

    Исправлены названия переменных

--- app/src/main/java/com/suai/recepies/SignUpActivity.kt
+++ app/src/main/java/com/suai/recepies/SignUpActivity.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,35 +22,37 @@ class SignUpActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)}
 
-        val user_login: EditText = findViewById(R.id.login_signup)
-        val user_password: EditText = findViewById(R.id.password_signup)
-        val user_nick_name: EditText = findViewById(R.id.nick_name)
-        val user_date_of_birth: EditText = findViewById(R.id.date_of_birth)
+        val userLogin: EditText = findViewById(R.id.login_signup)
+        val userPassword: EditText = findViewById(R.id.password_signup)
+        val userNickName: EditText = findViewById(R.id.nick_name)
+        val userDateOfBirth: EditText = findViewById(R.id.date_of_birth)
         val button: Button = findViewById(R.id.signup)
 
         button.setOnClickListener {
-            val login = user_login.text.toString().trim()
-            val password = user_password.text.toString().trim()
-            val nick_name = user_nick_name.text.toString().trim()
-            val date_of_birth = user_date_of_birth.text.toString().trim()
+            val login = userLogin.text.toString().trim()
+            val password = userPassword.text.toString().trim()
+            val nickName = userNickName.text.toString().trim()
+            val dateOfBirth = userDateOfBirth.text.toString().trim()
 
-            if (login.isEmpty() || password.isEmpty() || nick_name.isEmpty() || date_of_birth.isEmpty()) {  //login.contains('@')
+
+            if (login.isEmpty() || password.isEmpty() || nickName.isEmpty() || dateOfBirth.isEmpty()) {
+
                 Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_LONG).show()
             }
 
             else {
 
-                val user = User(login, password, nick_name, date_of_birth)
+                val user = User(login, password, nickName, dateOfBirth)
                 // Создание нового объекта (система регистрации)
                 val db = BD(this, null)
                 db.addUser(user)
                 Toast.makeText(this, "Пользователь $login добавлен", Toast.LENGTH_LONG).show()
 
                 // Очищение полей
-                user_login.text.clear()
-                user_password.text.clear()
-                user_nick_name.text.clear()
-                user_date_of_birth.text.clear()
+                userLogin.text.clear()
+                userPassword.text.clear()
+                userNickName.text.clear()
+                userDateOfBirth.text.clear()
             }
         }
     }
