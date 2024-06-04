eb4f59b3ab311018d966337a10d1ba20613d93b6
BinaryCodeN <nensijw3@gmail.com>
2024-06-04 18:13:00 +0300

    Шаблон для ДАТЫ, работу которой должен проверить Дима

--- app/src/main/java/com/suai/recepies/RegistrationActivity.kt
+++ app/src/main/java/com/suai/recepies/RegistrationActivity.kt
@@ -1,16 +1,16 @@
 package com.suai.recepies
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
-
-
-
+import java.util.Calendar
 
 
 class RegistrationActivity : AppCompatActivity() {
@@ -30,6 +30,40 @@ class RegistrationActivity : AppCompatActivity() {
         val userDateOfBirth: EditText = findViewById(R.id.date_of_birth)
         val button: Button = findViewById(R.id.signup)
 
+        userDateOfBirth.setOnClickListener{
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(
+                this,
+                { _, year, month, day ->
+                    val selectedDate = "$day.${month + 1}.$year"
+                    // Выполняем проверку ввода даты и сохраняем ее
+                    userDateOfBirth.setText(selectedDate)
+                },
+                year,
+                month,
+                day
+            )
+            datePickerDialog.show()
+            println("Проверка даты прошла успешно")
+        }
+
+// Выполняем проверку ввода даты при изменении текста
+        userDateOfBirth.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val inputDate = s.toString()
+                // Выполняем проверку ввода даты и сохраняем ее
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+
         button.setOnClickListener {
             val login = userLogin.text.toString().trim()
             val password = userPassword.text.toString().trim()
@@ -41,6 +75,7 @@ class RegistrationActivity : AppCompatActivity() {
 
                 Toast.makeText(this, "Все поля данной формы обязательны для заполнения", Toast.LENGTH_LONG).show()
             }
+
             else {
                 val registerHTTP: String = "http://10.0.2.2:5000/register"
                 val dictionary: Map<String, String> = mapOf("login" to login, "password" to password, "nick_name" to nickName, "date_of_birth" to dateOfBirth)
