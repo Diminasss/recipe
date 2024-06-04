630211b980f5efad705a33f44d5556da44177224
Diminas <pilluping@gmail.com>
2024-06-04 14:16:59 +0300

    уже не такая жалкая попытка работы с базой данных

--- app/src/main/java/com/suai/recepies/DatabaseManager.kt
+++ app/src/main/java/com/suai/recepies/DatabaseManager.kt
@@ -1,5 +1,7 @@
 package com.suai.recepies
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 
@@ -9,5 +11,34 @@ class DatabaseManager(context: Context) {
 
     fun openDB(){
         db = myDBHelper.writableDatabase
+    }
+    fun insertUserToDB(login: String, password: String, nickname: String, dateOfBirth: String, recipesOwner: String){
+        val values = ContentValues().apply {
+            put(MyBDNameClass.COLUMN_NAME_LOGIN, login)
+            put(MyBDNameClass.COLUMN_NAME_PASSWORD, password)
+            put(MyBDNameClass.COLUMN_NAME_NICKNAME, nickname)
+            put(MyBDNameClass.COLUMN_NAME_DATE_OF_BIRTH, dateOfBirth)
+            put(MyBDNameClass.COLUMN_NAME_RECIPES_OWNER, recipesOwner)
+        }
+        db?.insert(MyBDNameClass.TABLE_NAME, null, values)
+    }
+    @SuppressLint("Range")
+    fun getUserFromDB(): ArrayList<String>{
+        val dataList = ArrayList<String>()
+        val cursor = db?.query(MyBDNameClass.TABLE_NAME, null, null, null, null, null, null)
+
+        with(cursor){
+            while (cursor?.moveToNext()!!){
+                val dataText = cursor.getString(cursor.getColumnIndex(MyBDNameClass.COLUMN_NAME_LOGIN))
+                dataList.add(dataText.toString())
+            }
+        }
+        cursor?.close()
+
+        return dataList
+    }
+
+    fun closeDB(){
+        myDBHelper.close()
     }
 }
--- app/src/main/java/com/suai/recepies/DatabaseNameClass.kt
+++ app/src/main/java/com/suai/recepies/DatabaseNameClass.kt
@@ -4,12 +4,12 @@ package com.suai.recepies
 import android.provider.BaseColumns
 
 object MyBDNameClass{
-    private const val TABLE_NAME = "user"
-    private const val COLUMN_NAME_LOGIN = "login"
-    private const val COLUMN_NAME_PASSWORD = "password"
-    private const val COLUMN_NAME_NICKNAME = "nick_name"
-    private const val COLUMN_NAME_DATE_OF_BIRTH = "date_of_birth"
-    private const val COLUMN_NAME_RECIPES_OWNER = "recipes_owner"
+    const val TABLE_NAME = "user"
+    const val COLUMN_NAME_LOGIN = "login"
+    const val COLUMN_NAME_PASSWORD = "password"
+    const val COLUMN_NAME_NICKNAME = "nick_name"
+    const val COLUMN_NAME_DATE_OF_BIRTH = "date_of_birth"
+    const val COLUMN_NAME_RECIPES_OWNER = "recipes_owner"
 
     const val DATABASE_VERSION = 1
     const val DATABASE_NAME = "database.sqlite"
