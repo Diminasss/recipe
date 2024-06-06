367924c04886fa924b3c886870f9f6a8a5d2f5b0
BinaryCodeN <nensijw3@gmail.com>
2024-06-06 20:21:03 +0300

    Добавлены активити для отображения других блюд и своих блюд

---
+++ app/src/main/java/com/suai/recepies/ViewRecipesActivity.kt
@@ -0,0 +1,28 @@
+package com.suai.recepies
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ViewRecipesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_view_recipes)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val myRecipes:Button = findViewById(R.id.myrecipes)
+        myRecipes.setOnClickListener{
+            val intent = Intent(this, MyRecipesActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
