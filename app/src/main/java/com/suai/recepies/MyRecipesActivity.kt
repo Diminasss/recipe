6d1cb9d0dfd9c8edd49740fa49dda037f2af02d7
Diminas <pilluping@gmail.com>
2024-06-12 22:11:06 +0300

    Отменный скролвью

--- app/src/main/java/com/suai/recepies/MyRecipesActivity.kt
+++ app/src/main/java/com/suai/recepies/MyRecipesActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Base64
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -31,11 +33,6 @@ class MyRecipesActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-
-
-
-
-
         val login: String = databaseManager.getLoginFromDB()
         val recipes: List<Recipe> = doPostRetroMyRecipes(login, "http://10.0.2.2:5000/get_users_recipes/")
         println("Получено с сервера моих рецептов ${recipes.size}")
@@ -45,6 +42,19 @@ class MyRecipesActivity : AppCompatActivity() {
         val b = databaseManager.getAllIdsMyRecipesOnly()
         print("В базе данных теперь содержится: ${b.size} рецептов\n")
 
+        val recipesContainer: LinearLayout = findViewById(R.id.recipesContainer)
+        recipes.forEach { recipe ->
+            val recipeView = LayoutInflater.from(this).inflate(R.layout.recipe_item, recipesContainer, false)
 
+            val imageView: ImageView = recipeView.findViewById(R.id.recipe_image)
+            val titleView: TextView = recipeView.findViewById(R.id.recipe_title)
+            // Задайте изображение и текст для каждого рецепта
+            titleView.text = recipe.title
+            val imageBytes = Base64.decode(recipe.photo, Base64.DEFAULT)
+            val decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+            imageView.setImageBitmap(decodedImage)
+
+            recipesContainer.addView(recipeView)
+        }
     }
 }
